73f08b05fcbe672696a93a595857cf613331ea1c
howooking <junsgk@gmail.com>
2022-12-21 00:34:13 +0900

    로그인,랜덤끔,단어자료에 wrong,id추가

--- src/components/VocaList.tsx
+++ src/components/VocaList.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-shadow */
-import { ListItemButton, ListItemText } from '@mui/material';
+import { Alert, ListItemButton, ListItemText, Snackbar } from '@mui/material';
 import { useState } from 'react';
 import { Voca } from '../models/voca';
 
@@ -7,14 +7,19 @@ type VocaCardProps = {
   voca: Voca;
   i: number;
   answer: Voca | undefined;
+  handleNextPage: () => void;
 };
+
 const handleColor = () => {};
 
 export default function VocaList({
   voca,
   i,
   answer,
+  handleNextPage,
 }: VocaCardProps): JSX.Element {
+  const [open, setOpen] = useState(false);
+  const handleCloseCloseSnackbar = () => setOpen(false);
   const [isEnd, setIsEnd] = useState(false);
   const [isRight, setIsRight] = useState(false);
   const isAnswer = voca.id === answer?.id;
@@ -22,22 +27,47 @@ export default function VocaList({
     if (event.target.innerText.slice(3) === answer?.kor) {
       setIsRight(true);
       setIsEnd(true);
+      setOpen(true);
+      setTimeout(() => {
+        handleNextPage();
+      }, 2000);
     } else {
       setIsRight(false);
       setIsEnd(true);
+      setOpen(true);
+      setTimeout(() => {
+        handleNextPage();
+      }, 3000);
     }
   };
   console.log({ isRight, isEnd });
 
   return (
-    <ListItemButton
-      sx={{ bgcolor: 'background.paper', width: '600px' }}
-      onClick={(e) => handleAnswer(e, answer)}
-    >
-      <ListItemText
-        primary={`${i + 1}. ${voca.kor}`}
-        sx={{ color: isEnd && isAnswer ? 'red' : 'black' }}
-      />
-    </ListItemButton>
+    <>
+      <ListItemButton
+        sx={{ bgcolor: 'background.paper', width: '600px' }}
+        onClick={(e) => handleAnswer(e, answer)}
+      >
+        <ListItemText
+          primary={`${i + 1}. ${voca.kor}`}
+          sx={{ color: isEnd && isAnswer ? 'red' : 'black' }}
+        />
+      </ListItemButton>
+      <Snackbar
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+        open={open}
+        autoHideDuration={4000}
+        onClose={handleCloseCloseSnackbar}
+      >
+        <Alert
+          severity={isRight ? 'success' : 'error'}
+          onClose={handleCloseCloseSnackbar}
+          elevation={6}
+          sx={{ fontSize: 20, display: 'flex', alignItems: 'center' }}
+        >
+          {isRight ? '정답입니다😃😃' : '오답입니다😥😥 틀린단어에 추가됩니다.'}
+        </Alert>
+      </Snackbar>
+    </>
   );
 }
--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,18 +1,26 @@
 import { CircularProgress, IconButton, List, Stack } from '@mui/material';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import { useState } from 'react';
 import { Voca } from '../models/voca';
 import VocaList from '../components/VocaList';
 import useGetAllVocas from '../hooks/useGetAllVocas';
 import CardPaper from '../components/CardPaper';
 
 export default function Test(): JSX.Element {
+  const [page, setPage] = useState<number>(1);
+  const handleNextPage = () => {
+    setPage((prev) => prev + 1);
+  };
+  const handlePrevPage = () => {
+    setPage((prev) => prev - 1);
+  };
   const { isLoading, vocas } = useGetAllVocas();
-  function getMultipleRandom(num: number): Voca[] | undefined {
-    const shuffled = vocas && [...vocas].sort(() => 0.5 - Math.random());
-    return shuffled?.slice(0, num);
-  }
-  const randomVocas = getMultipleRandom(5);
+  // function getMultipleRandom(): Voca[] | undefined {
+  //   return vocas && [...vocas].sort(() => 0.5 - Math.random());
+  // }
+
+  const randomVocas = vocas?.slice(5 * (page - 1), 5 * page);
   const answer = randomVocas && randomVocas[Math.floor(Math.random() * 4) + 1];
   console.log({ rand5: randomVocas, answer });
 
@@ -22,16 +30,27 @@ export default function Test(): JSX.Element {
       <Stack alignItems="center">
         <CardPaper answer={answer} />
         <Stack direction="row" alignItems="center">
-          <IconButton sx={{ m: 2 }} size="large">
+          <IconButton
+            sx={{ m: 2 }}
+            size="large"
+            onClick={handlePrevPage}
+            disabled={page === 1}
+          >
             <ArrowBackIosIcon sx={{ fontSize: 30 }} />
           </IconButton>
 
           <List>
             {randomVocas?.map((voca: Voca, i: number) => (
-              <VocaList key={voca.id} voca={voca} i={i} answer={answer} />
+              <VocaList
+                key={voca.id}
+                voca={voca}
+                i={i}
+                answer={answer}
+                handleNextPage={handleNextPage}
+              />
             ))}
           </List>
-          <IconButton sx={{ m: 2 }} size="large">
+          <IconButton sx={{ m: 2 }} size="large" onClick={handleNextPage}>
             <ArrowForwardIosIcon sx={{ fontSize: 30 }} />
           </IconButton>
         </Stack>
